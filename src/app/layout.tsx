7af999bf5799fcaaa43123365f9981ffadc86e06
aniruddha3919 <aniruddhapal43@gmail.com>
2026-02-05 00:21:05 +0530

    google analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,20 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} antialiased`}>
         {children}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YY57E7XQ84"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YY57E7XQ84');
+          `}
+        </Script>
       </body>
     </html>
   );
